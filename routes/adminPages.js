8611f2359888ab1027ed5532c767de83c4602dd1
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-11 23:37:24 +0400

    update

--- routes/adminPages.js
+++ routes/adminPages.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+* GET pages index
+*/
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'CMS Cart Admin'
-    });
+    res.send('admin panel');
 });
+
+/*
+* GET add page
+*/
+router.get('/add-page', (req, res) => {
+    let title = "";
+    let slug = "";
+    let content = "";
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    })
+})
 
 
 module.exports = router;
